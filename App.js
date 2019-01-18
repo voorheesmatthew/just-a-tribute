8d6c9033d88ce312624bdd123be07e458a0474da
Matt Voorhees <voorhees.matthew@gmail.com>
2019-01-18 14:01:45 -0800

    add images and style

--- App.js
+++ App.js
@@ -9,6 +9,38 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+const Images = [
+  {
+    uri: "https://i.imgur.com/mxgtWKt.jpg",
+    label: "Cat on a blue blanket"
+  },
+
+  {
+    uri: "https://i.imgur.com/XCRnNWn.jpg",
+    label: "A cat toy"
+  },
+
+  {
+    uri: "https://i.imgur.com/dqQX1K0.jpg",
+    label: "A close up of a dog"
+  },
+
+  {
+    uri: "https://i.imgur.com/nZXbSbh.jpg",
+    label: "Sheep next to a cat"
+  },
+
+  {
+    uri: "https://i.imgur.com/mXCjefR.jpg",
+    label: "Cat laying on the grass"
+  },
+
+  {
+    uri: "https://i.imgur.com/AGyxRcc.jpg",
+    label: "Bird sitting on a railing"
+  }
+];
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -34,16 +66,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#abcdef'
   },
-  welcome: {
-    fontSize: 20,
+  image: {
+    flex: 2,
+    width: 320,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  imageLabel: {
     textAlign: 'center',
-    margin: 10,
+    backgroundColor: 'rgba(100, 100, 100, 0.5)',
+    color: 'white',
+    width: 320
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  empty: {
+    flex: 1
+  }
 });
